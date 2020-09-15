9e15bcd04fc47c74ae70fa1bc04567fabd566d38
John Youn <john@Johns-MacBook-Pro.local>
2020-09-15 00:14:55 -0700

    almost

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Link, useRouteMatch, useHistory } from 'react-router-dom';
 
 export default function MovieList(props) {
+  const { url } = useRouteMatch();
+  console.log('the url', url);
+  const history = useHistory();
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {props.movies.map((movie) => (
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  console.log(props);
+  const { title, director, metascore, id } = props.movie;
 
   return (
     <div className="movie-card">
